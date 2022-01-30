10534b40d18f4410927f9e812c2a4c0df28e4fc0
Kartik Kanungo <kartikkanungo22@gmail.com>
2022-01-30 13:22:28 +0530

    Add window framework

---
+++ Baate/Modules/Window/Window.h
@@ -0,0 +1,18 @@
+//
+//  Window.h
+//  Window
+//
+//  Created by Kartik Kanungo on 30/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Window.
+FOUNDATION_EXPORT double WindowVersionNumber;
+
+//! Project version string for Window.
+FOUNDATION_EXPORT const unsigned char WindowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Window/PublicHeader.h>
+
+
