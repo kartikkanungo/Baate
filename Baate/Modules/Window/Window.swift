df1bd62606b81cbee6047b67dcc7e5c2d577bcfb
Kartik Kanungo <kartikkanungo22@gmail.com>
2022-01-30 13:25:35 +0530

    Create a window

---
+++ Baate/Modules/Window/Window.swift
@@ -0,0 +1,20 @@
+//
+//  Window.swift
+//  Window
+//
+//  Created by Kartik Kanungo on 30/01/22.
+//
+
+import Foundation
+import UIKit
+
+class Window: UIWindow {
+    override init(windowScene: UIWindowScene) {
+        super.init(windowScene: windowScene)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
